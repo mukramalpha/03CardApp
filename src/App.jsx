af7b3ad30fe641b2561110a19be57792cb37b295
mukramalpha <mukram.alphatech@gmail.com>
2024-02-29 16:21:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Card } from './Components/Card'
+import { Card1 } from './Components/Card1'
+
+function App() {
+
+
+  return (
+    <>
+      <Card name="Macbook"/>
+      <Card1 username="Clinton" btnText="View Profile"/>
+      <Card1 username="Rani" />
+    </>
+  )
+}
+
+export default App
